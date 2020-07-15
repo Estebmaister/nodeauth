8576fc4ba53dc337f97beab8da2ee6be43146626
Esteban Camargo <estebmaister@gmail.com>
2020-07-15 00:40:56 -0400

    ✨ mail configuration

--- app/mail.js
+++ app/mail.js
@@ -2,7 +2,7 @@ const config = require("./config.js");
 const nodemailer = require("nodemailer");
 
 // async ...await is not allowed in global scope, must use a wrapper
-const sendMail = async (emailAddressee) => {
+const sendMail = async (emailAddressee, userID) => {
   // Generate test SMTP service account from ethereal.email
   // Only needed if you don't have a real mail account for testing
   // let testAccount = await nodemailer.createTestAccount();
@@ -26,24 +26,27 @@ const sendMail = async (emailAddressee) => {
   });
 
   // send mail with defined transport object
-  const passForgottenMail = (receiver = "estebmaister@gmail.com") => ({
+  const passForgottenMail = (
+    receiver = "estebmaister@gmail.com",
+    queryURL = ""
+  ) => ({
     from: `"Esteban Camargo NODE SERVER" <${config.EMAIL_USER}>`, // sender address
     to: `${receiver}`, // list of receivers
     subject: "Password recuperation ✔", // Subject line
     text: `We have received a solicitude to reestablish your password in our server, 
-          for establish another password you can access this link ${"--link--"}\n
+          for establish another password you can access this link ${config.PROJECT_URL}\n
           If you didn't make this solicitude, you can delete this message.`, // plain text body
     html: `<b>We have received a solicitude to reestablish your password in our server, 
           for establish another password you can access the following link:</b>
           <br>
-          ${"https://nodeauth-esteb.glitch.me"}
+          ${config.PROJECT_URL}/reset?userID=${queryURL}
           <br>
           <br>
           If you didn't make this solicitude, you can delete this message.`, // html body
   });
 
   let info = await transporter
-    .sendMail(passForgottenMail(emailAddressee))
+    .sendMail(passForgottenMail(emailAddressee, userID))
     .catch((err) => console.log("Error sending message:", err));
 
   console.log("Message sent: %s", info.messageId);
