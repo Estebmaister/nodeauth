098a2811e336a6a25dbcad3f536b5cd66c61308f
Esteban Camargo <estebmaister@gmail.com>
2020-07-15 00:42:11 -0400

    💄♻️ for password checking

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,6 @@
 // ___ Register Form Functionalities ___ //
 
 // ___  Switch between LOGIN & REGISTER FORM ___ //
-
 const aLogin = document.querySelector("#a-login");
 if (aLogin) {
   const aRegister = document.querySelector("#a-register");
@@ -19,65 +18,54 @@ if (aLogin) {
   });
 }
 
-// Show-hide password all icon-show class
+// ___ Show-hide password all icon-show class ___ //
 const iconsShow = document.getElementsByClassName("icon-show");
-const pathsHide = document.getElementsByClassName("path-hide");
-const pathsShow = document.getElementsByClassName("path-show");
-for (let index = 0; index < iconsShow.length; index++) {
-  iconsShow[index].addEventListener("mousedown", () => {
-    iconsShow[index].previousSibling.type = "text";
-    pathsHide[index].style.display = "block";
-    pathsShow[index].style.display = "none";
-  });
-  iconsShow[index].addEventListener("mouseup", () => {
-    iconsShow[index].previousSibling.type = "password";
-    pathsHide[index].style.display = "none";
-    pathsShow[index].style.display = "block";
-  });
+if (iconsShow) {
+  const pathsHide = document.getElementsByClassName("path-hide");
+  const pathsShow = document.getElementsByClassName("path-show");
+  for (let index = 0; index < iconsShow.length; index++) {
+    iconsShow[index].addEventListener("mousedown", () => {
+      iconsShow[index].previousSibling.type = "text";
+      pathsHide[index].style.display = "block";
+      pathsShow[index].style.display = "none";
+    });
+    iconsShow[index].addEventListener("mouseup", () => {
+      iconsShow[index].previousSibling.type = "password";
+      pathsHide[index].style.display = "none";
+      pathsShow[index].style.display = "block";
+    });
+  }
 }
 
-// Check CapsLock activated
-const regPassword = document.getElementById("reg-password");
-const regCapsLockMessage = document.getElementById("reg-caps-message");
-
-regPassword.addEventListener("keyup", (event) => {
-  // If "caps lock" is pressed, display the warning text
-  try {
-    if (event.getModifierState("CapsLock")) {
-      regCapsLockMessage.innerHTML = "Caps lock is ON";
-      regCapsLockMessage.style.color = "red";
-    } else {
-      regCapsLockMessage.innerHTML = "";
-    }
-  } catch (error) {
-    console.log("Can´t check capsLock in #reg-password");
+// ___ Check CapsLock activated ___ //
+const resPassword = document.querySelectorAll(".password-field");
+if (resPassword) {
+  const resCapsLockMessage = document.querySelectorAll(".caps-message");
+  for (let index = 0; index < resPassword.length; index++) {
+    resPassword[index].addEventListener("keyup", (event) => {
+      // If "caps lock" is pressed, display the warning text
+      try {
+        if (event.getModifierState("CapsLock")) {
+          resCapsLockMessage[index].innerHTML = "Caps lock is ON";
+          resCapsLockMessage[index].style.color = "red";
+        } else {
+          resCapsLockMessage[index].innerHTML = "";
+        }
+      } catch (error) {
+        console.log(`Can´t check capsLock in ${resPassword[index]}`);
+      }
+    });
   }
-});
+}
 
-const logPassword = document.getElementById("log-password");
-const logCapsLockMessage = document.getElementById("log-caps-message");
-
-logPassword.addEventListener("keyup", (event) => {
-  // If "caps lock" is pressed, display the warning text
-  try {
-    if (event.getModifierState("CapsLock")) {
-      logCapsLockMessage.innerHTML = "Caps lock is ON";
-      logCapsLockMessage.style.color = "red";
-    } else {
-      logCapsLockMessage.innerHTML = "";
-    }
-  } catch (error) {
-    console.log("Can´t check capsLock in #log-password");
-  }
-});
-
-// Check confirm password
-const confirmPassword = document.getElementById("confirm-password");
-const confirmMessage = document.getElementById("confirm-message");
+// ___ Check confirm password ___ //
+const confirmPassword = document.querySelector(".confirm-password");
+const confirmMessage = document.querySelector(".confirm-message");
+const checkPassword = document.querySelector(".check-password");
 const check = () => {
   // If some value is added in confirmP o the password changes,
   // display the matching text
-  if (regPassword.value == confirmPassword.value) {
+  if (checkPassword.value == confirmPassword.value) {
     confirmMessage.style.color = "green";
     confirmMessage.innerHTML = "matching";
     confirmPassword.setCustomValidity("");
